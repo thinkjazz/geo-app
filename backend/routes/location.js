7ba6e214020b004ec48d52504316838c05036940
lookrecius <bragin.boris@ya.ru>
2020-09-29 02:00:11 +0300

    Add router connection

--- backend/routes/location.js
+++ backend/routes/location.js
@@ -9,21 +9,28 @@ const locationStorage = {
 router.post('/add-location-point',
     (request,
      response, next)=> {
-
+    const randomIdHandler = Math.random() * 10;
     locationStorage.location.push({
-      id: Math.random() * 10,
+      id: randomIdHandler,
       address: request.body.address,
       coords: { lat: request.body.lat, lng: request.body.lng }
     });
-    response.json({ message: 'Stored in db'});
+    response.json({ message: 'Stored location', locId: randomIdHandler });
 
 });
 
-router.get('/location',
+router.get('/location/:lid',
     (request,
      response,
      next)=> {
-
+    const locationId = +request.params.lid;
+    const location = locationStorage.location.find(loc => {
+        return loc.id === locationId;
+    });
+    if (!location) {
+        return response.status(404).json({message: 'Not found!'})
+    }
+    response.json({address: location.address, coordinates: location.coords})
 });
 
 module.exports = router;
