4a9592a96654da0ab01af314a6f8d5515e967abf
lookrecius <bragin.boris@ya.ru>
2020-09-28 03:09:36 +0300

    Added: backend routes/location.js

---
+++ backend/routes/location.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const router = express.Router();
+
+const locationStorage = {
+  location: []
+};
+
+router.post('/add-location-point', (request, response, next)=> {
+
+    locationStorage.location.push({
+      id: 'uid' + Math.random() * 10,
+      address: request.body.address,
+      coords: { lat: request.body.lat, lng: request.body.lng }
+    });
+    response.json({ message: 'Stored in db'});
+
+});
+
+
+
+router.get('/location', (request, response, next)=> {
+
+});
+
+module.exports = router;
